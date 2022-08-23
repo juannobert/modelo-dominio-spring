0221a71a724d1657a7adc9399a8c6be25ccd12d2
Juan Nobert <nobert407@gmail.com>
2022-08-23 12:15:45 -0300

    Find all products implemented

--- src/main/java/com/juannobert/modelo/services/ProductService.java
+++ src/main/java/com/juannobert/modelo/services/ProductService.java
@@ -1,8 +1,13 @@
 package com.juannobert.modelo.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.juannobert.modelo.dto.ProductDTO;
+import com.juannobert.modelo.entities.Product;
 import com.juannobert.modelo.repositories.ProductRepository;
 
 @Service
@@ -10,4 +15,13 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
+	
+	public List<ProductDTO> findAll(){
+		List<Product> list = repository.findAll();
+		return list.stream()
+				.map(x -> new ProductDTO(x))
+				.collect(Collectors.toList());
+	}
+	
+
 }
