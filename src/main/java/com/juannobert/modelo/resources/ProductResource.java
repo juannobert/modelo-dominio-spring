2cc3e067f39eb20e2f762d158d31d0710e4c0b75
Juan Nobert <nobert407@gmail.com>
2022-08-24 13:56:16 -0300

    Product resource and find all implemented

---
+++ src/main/java/com/juannobert/modelo/resources/ProductResource.java
@@ -0,0 +1,27 @@
+package com.juannobert.modelo.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.juannobert.modelo.dto.ProductDTO;
+import com.juannobert.modelo.services.ProductService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductResource {
+	
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping
+	public ResponseEntity<List<ProductDTO>> findAll(){
+		List<ProductDTO> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+
+}
