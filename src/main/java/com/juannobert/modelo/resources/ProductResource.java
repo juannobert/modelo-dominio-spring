51bc6a3925ab41fa2caeacea6617995db74b2834
Juan Nobert <nobert407@gmail.com>
2022-08-24 14:06:39 -0300

    Insert new product implemented

--- src/main/java/com/juannobert/modelo/resources/ProductResource.java
+++ src/main/java/com/juannobert/modelo/resources/ProductResource.java
@@ -1,12 +1,16 @@
 package com.juannobert.modelo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.juannobert.modelo.dto.ProductDTO;
 import com.juannobert.modelo.services.ProductService;
@@ -24,4 +28,13 @@ public class ProductResource {
 		return ResponseEntity.ok(list);
 	}
 
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	
 }
--- src/main/java/com/juannobert/modelo/services/ProductService.java
+++ src/main/java/com/juannobert/modelo/services/ProductService.java
@@ -3,8 +3,10 @@ package com.juannobert.modelo.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.juannobert.modelo.dto.ProductDTO;
 import com.juannobert.modelo.entities.Product;
@@ -16,6 +18,7 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<ProductDTO> findAll(){
 		List<Product> list = repository.findAll();
 		return list.stream()
@@ -23,5 +26,13 @@ public class ProductService {
 				.collect(Collectors.toList());
 	}
 	
+	
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		BeanUtils.copyProperties(dto, entity);
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
 
 }
